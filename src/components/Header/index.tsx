db16015d93c456759fe5e4a4cb7a433ace65a900
EduGollner0609 <dudugollner@gmail.com>
2024-11-30 00:59:49 -0300

    Estilizando header.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,27 @@
 import './styles.css';
 import logo from '../../assets/logo.jpg';
+import { NavLink } from 'react-router-dom';
 
 
 export default function Header() {
     return (
         <header>
-            <nav>
+            <nav className="container">
                 <img src={logo} alt="" />
+                <div className="options-navegations">
+                    <NavLink to="">
+                        Início
+                    </NavLink>
+                    <NavLink to="">
+                        Sobre
+                    </NavLink>
+                    <NavLink to="">
+                        Serviços
+                    </NavLink>
+                    <NavLink to="">
+                        Contato
+                    </NavLink>
+                </div>
             </nav>
         </header>
     );
