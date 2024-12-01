adf818469877c756ff70231d257b3e33a470d657
EduGollner0609 <dudugollner@gmail.com>
2024-12-01 04:02:26 -0300

    Criando rota ServicePage.

---
+++ src/routes/Page/ServicePage/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+export default function ServicePage() {
+    return (
+        <main>
+            <section id="section-services-page">
+
+            </section>
+        </main>
+    );
+}
